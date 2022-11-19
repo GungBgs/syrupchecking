70dfd96c555c783d6d03b1c5acd280cdaa7e6847
gungbgs <agungbagus2498@gmail.com>
2022-11-20 03:50:14 +0800

    add google analytic

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import Main from "./pages/Main";
+import ReactGA from 'react-ga';
+const TRACKING_ID = "G-C978G4BY60"; // OUR_TRACKING_ID
 
 function App() {
+  ReactGA.initialize(TRACKING_ID);
   return <Main />;
 }
 
